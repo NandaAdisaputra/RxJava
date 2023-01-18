1e38a10252914e928d61ecb90de04587590dabc3
NandaAdisaputra <nandaadisaputra18@gmail.com>
2023-01-19 06:12:16 +0700

    Latihan ReactiveX RXJava

--- app/src/main/java/com/nandaadisaputra/reactivexrxjava/MainActivity.kt
+++ app/src/main/java/com/nandaadisaputra/reactivexrxjava/MainActivity.kt
@@ -1,112 +1,96 @@
 package com.nandaadisaputra.reactivexrxjava
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.Patterns
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.jakewharton.rxbinding2.widget.RxTextView
 import com.nandaadisaputra.reactivexrxjava.databinding.ActivityMainBinding
+import io.reactivex.Observable
 
 class MainActivity : AppCompatActivity() {
 
-    private var emailValid = false
-    private var passwordValid = false
-    private var passwordConfirmationValid = false
+    private lateinit var binding: ActivityMainBinding
 
-    lateinit var binding: ActivityMainBinding
-
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //Menambahkan data stream dari masing masing inputan, kemudian subscribe ke masing
+        //masing stream tersebut.
 
-        validateButton()
-
-        binding.edEmail.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
+        //Kita menggunakan RxTextView.textChanges(binding.edEmail) untuk membaca setiap perubahan
+        // pada EditText dan mengubahnya menjadi data stream.
+        val emailStream = RxTextView.textChanges(binding.edEmail)
+            //operator skipInitialValue() untuk menghiraukan input awal. Hal ini bertujuan
+            // supaya aplikasi tidak langsung menampilkan eror pada saat pertama kali dijalankan.
+            .skipInitialValue()
+            // Kita akan menggunakan map. Jika format tidak valid maka ia akan mengembalikan nilai TRUE.
+            // Kemudian saat subscribe, Jika hasilnya TRUE kita memanggil fungsi showEmailExistAlert(it)
+            // untuk menampilkan peringatannya
+            .map { email ->
+                !Patterns.EMAIL_ADDRESS.matcher(email).matches()
             }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                validateEmail()
-            }
-        })
-
-        binding.edPassword.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                validatePassword()
-            }
-        })
-
-        binding.edConfirmPassword.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                validatePasswordConfirmation()
-            }
-        })
-    }
-
-    fun validateEmail() {
-        // jika password tidak valid tampilkan peringatan
-        val input = binding.edEmail.text.toString()
-        if (!Patterns.EMAIL_ADDRESS.matcher(input).matches()) {
-            emailValid = false
-            showEmailExistAlert(true)
-        } else {
-            emailValid = true
-            showEmailExistAlert(false)
+        emailStream.subscribe {
+            showEmailExistAlert(it)
         }
-        validateButton()
-    }
 
-    fun validatePassword() {
-        // jika password < 6 karakter tampilkan peringatan
-        val input = binding.edPassword.text.toString()
-        if (input.length < 6) {
-            passwordValid = false
-            showPasswordMinimalAlert(true)
-        } else {
-            passwordValid = true
-            showPasswordMinimalAlert(false)
+        val passwordStream = RxTextView.textChanges(binding.edPassword)
+            .skipInitialValue()
+            .map { password ->
+                password.length < 6
+            }
+        passwordStream.subscribe {
+            showPasswordMinimalAlert(it)
         }
-        validateButton()
-    }
-
-    fun validatePasswordConfirmation() {
-        // jika konfirmasi password tidak sesuai tampilkan peringatan
-        val input = binding.edConfirmPassword.text.toString()
-        if (input != binding.edPassword.text.toString()) {
-            passwordConfirmationValid = false
-            showPasswordConfirmationAlert(true)
-        } else {
-            passwordConfirmationValid = true
-            showPasswordConfirmationAlert(false)
+        //Pada bagian inputan confirmation password akan sedikit berbeda.
+        // Karena mengecek pada dua inputan sekaligus, yaitu pada
+        // ed_password dan ed_confirm_password. Sehingga perlu menggabungkan
+        // dua data tersebut dengan operator merge.
+        val passwordConfirmationStream = Observable.merge(
+            RxTextView.textChanges(binding.edPassword)
+                .map { password ->
+                    password.toString() != binding.edConfirmPassword.text.toString()
+                },
+            RxTextView.textChanges(binding.edConfirmPassword)
+                .map { confirmPassword ->
+                    confirmPassword.toString() != binding.edPassword.text.toString()
+                }
+        )
+        passwordConfirmationStream.subscribe {
+            showPasswordConfirmationAlert(it)
         }
-        validateButton()
-    }
-
-    private fun validateButton() {
-        // jika semua field sudah terisi, enable button submit
-        if (emailValid && passwordValid && passwordConfirmationValid) {
-            binding.btnRegister.isEnabled = true
-            binding.btnRegister.setBackgroundColor(ContextCompat.getColor(this, R.color.purple_500))
-        } else {
-            binding.btnRegister.isEnabled = false
-            binding.btnRegister.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray))
+        //Dengan operator combineLatest Kita akan membaca ketiga data stream tersebut untuk menentukan
+        //apakah tombol diaktifkan atau tidak
+        val invalidFieldsStream = Observable.combineLatest(
+            emailStream,
+            passwordStream,
+            passwordConfirmationStream
+        ) { emailInvalid: Boolean, passwordInvalid: Boolean, passwordConfirmationInvalid: Boolean ->
+            !emailInvalid && !passwordInvalid && !passwordConfirmationInvalid
+        }
+        //Jika pada operator combineLatest kita menggabungkan dan mengubah data di dalamnya.
+        // Maka pada operator merge kita hanya menggabungkan datanya saja.
+        invalidFieldsStream.subscribe { isValid ->
+            if (isValid) {
+                binding.btnRegister.isEnabled = true
+                binding.btnRegister.setBackgroundColor(
+                    ContextCompat.getColor(
+                        this,
+                        R.color.purple_500
+                    )
+                )
+            } else {
+                binding.btnRegister.isEnabled = false
+                binding.btnRegister.setBackgroundColor(
+                    ContextCompat.getColor(
+                        this,
+                        android.R.color.darker_gray
+                    )
+                )
+            }
         }
     }
 
@@ -119,6 +103,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showPasswordConfirmationAlert(isNotValid: Boolean) {
-        binding.edConfirmPassword.error = if (isNotValid) getString(R.string.password_not_same) else null
+        binding.edConfirmPassword.error =
+            if (isNotValid) getString(R.string.password_not_same) else null
     }
 }
